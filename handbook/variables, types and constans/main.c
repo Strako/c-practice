c90a9fa2d49ed7902682ca75966f5b0be82085a9
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2025-06-04 13:51:40 -0600

    leason(1,2): variables and types, constants

---
+++ handbook/variables, types and constans/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//constant declared with define syntax (not use costant keyword or explicit type)
+#define JOSE_NAME "Jose ricardo carmona sanchez"
+
+int main(void){
+    //constant declared with keyword and explicit type
+    const char ARMANDO_NAME[] = "Armando isai hernandez ibarra";
+    
+    printf("First constant: %s \nSecond constant (define) %s\n", ARMANDO_NAME, JOSE_NAME);
+
+    float age = 18;
+    age = age*2.5;
+
+    printf("Size of char = %lu bytes \n", sizeof(char));
+    printf("Size of int = %lu bytes \n", sizeof(int));
+    printf("Size of short = %lu bytes \n", sizeof(short));
+    printf("Size of long = %lu bytes \n", sizeof(long));
+    printf("Size of float = %lu bytes \n", sizeof(float));
+    printf("Size of double = %lu bytes \n", sizeof(double));
+    printf("Size of long double = %lu bytes \n", sizeof(long double));
+}
